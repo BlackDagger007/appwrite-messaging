7d3d5d3b3c388f00a68a6c50c172ebbe1179a74f
Wahab <mailwahab247@gmail.com>
2024-08-11 22:40:49 +0000

    bug fix

--- src/App.js
+++ src/App.js
@@ -23,58 +23,58 @@ const App = () => {
     } catch (error) {
       console.error(error)
     }
-
-    const UpdateUser = (e) => {
-      setUser({ ...user, [e.target.name]: e.target.value })
-    }
-    console.log(user)
-
-    return (
-      <div className="auth-modal">
-        <div className="close-modal">🚫</div>
-        <h2>Sign Up</h2>
-        <p>By signing up you agree to our terms and conditions for using the app.</p>
-        <form onSubmit={RegisterUser}>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Email"
-            onChange={UpdateUser}
-            value={user?.email}
-          />
-          <input
-            type="tel"
-            id="tel"
-            name="tel"
-            placeholder="Phone Number"
-            onChange={UpdateUser}
-            value={user?.tel}
-          />
-          <label>
-            <input type="checkbox" required />
-            I agree to the terms and conditions
-          </label>
-          <input type="submit" className="form-button" />
-
-        </form>
-
-        <hr />
-
-        <h2>Download the app</h2>
-
-        <div className="link-container">
-          <a href="https://play.google.com/store">
-            <img src={googlePlayStoreLogo} alt="google play store icon" />
-          </a>
-          <a href="https://apps.apple.com/">
-            <img src={appleStoreLogo} alt="apple store icon" />
-          </a>
-        </div>
-
-      </div>
-    );
   }
+
+  const UpdateUser = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
+  console.log(user)
+
+  return (
+    <div className="auth-modal">
+      <div className="close-modal">🚫</div>
+      <h2>Sign Up</h2>
+      <p>By signing up you agree to our terms and conditions for using the app.</p>
+      <form onSubmit={RegisterUser}>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          onChange={UpdateUser}
+          value={user?.email}
+        />
+        <input
+          type="tel"
+          id="tel"
+          name="tel"
+          placeholder="Phone Number"
+          onChange={UpdateUser}
+          value={user?.tel}
+        />
+        <label>
+          <input type="checkbox" required />
+          I agree to the terms and conditions
+        </label>
+        <input type="submit" className="form-button" />
+
+      </form>
+
+      <hr />
+
+      <h2>Download the app</h2>
+
+      <div className="link-container">
+        <a href="https://play.google.com/store">
+          <img src={googlePlayStoreLogo} alt="google play store icon" />
+        </a>
+        <a href="https://apps.apple.com/">
+          <img src={appleStoreLogo} alt="apple store icon" />
+        </a>
+      </div>
+
+    </div>
+  );
 }
 
 export default App
